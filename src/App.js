6ecee7b078306820f82c3dc257de7bf33189f7a6
Emil Nilsson <52598265+EmilNilsson12@users.noreply.github.com>
2021-07-17 14:47:10 +0200

    Add navbar using Link from react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import './App.css';
 import HomePage from './pages/HomePage';
+import NavBar from './partials/NavBar';
+import AboutPage from './pages/AboutPage';
+import ArticlesList from './pages/ArticlesList';
+import ArticlePage from './pages/ArticlePage';
+
 import About from './pages/About';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
@@ -7,8 +12,14 @@ function App() {
 	return (
 		<Router>
 			<div className='App'>
-				<Route path='/' component={HomePage} exact />
-				<Route path='/about' component={About} />
+				<NavBar />
+				<div id='page-body'>
+					<Route path='/' component={HomePage} exact />
+					<Route path='/about' component={About} />
+					<Route path='/about-us' component={AboutPage} />
+					<Route path='/articles-list' component={ArticlesList} />
+					<Route path='/article' component={ArticlePage} />
+				</div>
 			</div>
 		</Router>
 	);
---
+++ src/partials/NavBar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const NavBar = () => {
+	return (
+		<>
+			<ul>
+				<li>
+					<Link to='/'>Home</Link>
+				</li>
+				<li>
+					<Link to='/articles-list'>Articles</Link>
+				</li>
+				<li>
+					<Link to='/about-us'>About Us</Link>
+				</li>
+				<li>
+					<Link to='/about'>About Me(the creator)</Link>
+				</li>
+			</ul>
+		</>
+	);
+};
+
+export default NavBar;
