c9deede537b428afc8a8abf6cd56a5ec44be64a8
Emil Nilsson <52598265+EmilNilsson12@users.noreply.github.com>
2021-07-17 14:23:28 +0200

    Create HomePage component

---
+++ src/pages/HomePage.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const HomePage = () => {
+	return (
+		<>
+			<h1>Hello, welcome to my blog!</h1>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem
+				esse harum reprehenderit dolorum praesentium sequi ad suscipit commodi
+				dicta hic quam voluptate expedita consequuntur eius, sapiente iste
+				molestiae a magni!
+			</p>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem
+				esse harum reprehenderit dolorum praesentium sequi ad suscipit commodi
+				dicta hic quam voluptate expedita consequuntur eius, sapiente iste
+				molestiae a magni!
+			</p>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem
+				esse harum reprehenderit dolorum praesentium sequi ad suscipit commodi
+				dicta hic quam voluptate expedita consequuntur eius, sapiente iste
+				molestiae a magni!
+			</p>
+		</>
+	);
+};
+
+export default HomePage;
