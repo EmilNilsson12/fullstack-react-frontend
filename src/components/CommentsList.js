0fba734cca0ecb680ac7821209eb54cd9d62de37
Emil Nilsson <52598265+EmilNilsson12@users.noreply.github.com>
2021-07-18 18:18:00 +0200

    Make newer comments appear first

--- src/components/CommentsList.js
+++ src/components/CommentsList.js
@@ -3,7 +3,7 @@ import React from 'react';
 const CommentsList = ({ comments }) => (
 	<>
 		<h3>Comments:</h3>
-		{comments.map((comment, key) => (
+		{[...comments].reverse().map((comment, key) => (
 			<div key={key}>
 				<h4>{comment.userName}</h4>
 				<p>{comment.text}</p>
